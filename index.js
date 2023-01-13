c6313fdd4f7c2899cde165202db0f9b669a15d9a
Anurag Guha <rick.anurag2@gmail.com>
2023-01-13 18:55:14 +0530

    Added images for respective choice

--- index.js
+++ index.js
@@ -8,18 +8,24 @@ scorePlayer.textContent+=" "+0
 scoreComputer.textContent+=" "+0
 let countPlayer=0, countComputer=0
 let compChoice
+let playerImg= document.getElementById("player-img")
+let computerImg= document.getElementById("computer-img")
 function rockClick(){
     if(isAlive===true){
+        playerImg.innerHTML="<img class=\"choice-img\" src=\"images\\rock.png\" alt=\"Your choice\">"
         compChoice=Math.floor(Math.random()*3)
         if(game[compChoice]==="rock"){
+            computerImg.innerHTML="<img class=\"choice-img\" src=\"images\\rock.png\" alt=\"Computer's choice\">"
             outcomeDetail.textContent="Rock ties with rock"
             outcome.textContent="It's a tie!"
         }else if(game[compChoice]==="paper"){
+            computerImg.innerHTML="<img class=\"choice-img\" src=\"images\\paper.png\" alt=\"Computer's choice\">"
             outcomeDetail.textContent="Rock is beaten by paper"
             outcome.textContent="You lost!"
             countComputer++
             scoreComputer.textContent= "Computer: "+countComputer
         }else{
+            computerImg.innerHTML="<img class=\"choice-img\" src=\"images\\scissor.png\" alt=\"Computer's choice\">"
             outcomeDetail.textContent="Rock beats scissors"
             outcome.textContent="You won!"
             countPlayer++
@@ -32,16 +38,20 @@ function rockClick(){
 }
 function paperClick(){
     if(isAlive===true){
+        playerImg.innerHTML="<img class=\"choice-img\" src=\"images\\paper.png\" alt=\"Your choice\">"
         compChoice=Math.floor(Math.random()*3)
         if(game[compChoice]==="paper"){
+            computerImg.innerHTML="<img class=\"choice-img\" src=\"images\\paper.png\" alt=\"Computer's choice\">"
             outcomeDetail.textContent="Paper ties with paper"
             outcome.textContent="It's a tie!"
         }else if(game[compChoice]==="scissor"){
+            computerImg.innerHTML="<img class=\"choice-img\" src=\"images\\scissor.png\" alt=\"Computer's choice\">"
             outcomeDetail.textContent="Paper is beaten by scissor"
             outcome.textContent="You lost!"
             countComputer++
             scoreComputer.textContent= "Computer: "+countComputer
         }else{
+            computerImg.innerHTML="<img class=\"choice-img\" src=\"images\\rock.png\" alt=\"Computer's choice\">"
             outcomeDetail.textContent="Paper beats rock"
             outcome.textContent="You won!"
             countPlayer++
@@ -54,16 +64,20 @@ function paperClick(){
 }
 function scissorClick(){
     if(isAlive===true){
+        playerImg.innerHTML="<img class=\"choice-img\" src=\"images\\scissor.png\" alt=\"Your choice\">"
         compChoice=Math.floor(Math.random()*3)
         if(game[compChoice]==="scissor"){
+            computerImg.innerHTML="<img class=\"choice-img\" src=\"images\\scissor.png\" alt=\"Computer's choice\">"
             outcomeDetail.textContent="Scissor ties with scissor"
             outcome.textContent="It's a tie!"
         }else if(game[compChoice]==="rock"){
+            computerImg.innerHTML="<img class=\"choice-img\" src=\"images\\rock.png\" alt=\"Computer's choice\">"
             outcomeDetail.textContent="Scissor is beaten by rock"
             outcome.textContent="You lost!"
             countComputer++
             scoreComputer.textContent= "Computer: "+countComputer
         }else{
+            computerImg.innerHTML="<img class=\"choice-img\" src=\"images\\paper.png\" alt=\"Computer's choice\">"
             outcomeDetail.textContent="Scissor beats paper"
             outcome.textContent="You won!"
             countPlayer++
